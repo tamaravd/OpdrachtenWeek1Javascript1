b188571ba14c8638197ef98f54030362eea154a6
tamaravd <63185522+tamaravd@users.noreply.github.com>
2020-05-05 11:11:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+    //this is my first js code//
+let name = 'Tamara';
+let number1 = 10;
+let number2 = 20;
+let age = 45;
+console.log('Hello Winc Academy');
+console.log(name);
+console.log(number1+number2);
+console.log(number1-number2);
+console.log(number1*number2);
+console.log(number1/number2);
+console.log(number1%number2);
+age="45";
+typeof(age);   
+
+console.log("Hello Winc Academy");
+VM97:1 Hello Winc Academy
+undefined
+let name = 'Tamara';
+undefined
+console.log(name);
+VM173:1 Tamara
+undefined
+let nubmer1= 30;
+undefined
+let number2 = 10;
+undefined
+console.log(number1+number2);
+VM295:1 Uncaught ReferenceError: number1 is not defined
+    at <anonymous>:1:13
+(anonymous) @ VM295:1
+let number1=30;
+undefined
+console.log(number1-number2);
+VM399:1 20
+undefined
+console.log(number1-number2);
+VM442:1 20
+undefined
+console.log(number1+number2);
+VM466:1 40
+undefined
+console.log(number1*number2);
+VM501:1 300
+undefined
+console.log(number1/number2);
+VM558:1 3
+undefined
+console.log(number1%number2);
+VM606:1 0
+undefined
+let age = 45;
+undefined
+age="45";
+"45"
+typeof age;
+"string"
