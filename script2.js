7e861a51af36ed7a317bf6bc4f8d6075d8104553
tamaravd <63185522+tamaravd@users.noreply.github.com>
2020-05-05 13:47:10 +0200

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,10 @@
+console.log('Hello World');
+/*dit is een grote som
+var numb1 = 1230941;
+var numb2 = 1823794;
+let numb3 = numb1 * numb2;
+console.log(numb3);
+var numb4 = 637263 / 54;
+console.log(numb4);*/
+
+
